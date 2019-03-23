5f6b747bfa78fcb695c0e5b893b47257fb580d99
Susmit <susmit.py@gmail.com>
2019-03-23 15:18:00 +0530

    middle

--- web_hook.py
+++ web_hook.py
@@ -1,7 +1,9 @@
 from flask import Flask,request,jsonify
+import requests
 
 app=Flask(__name__)
 token='Token'
+url = "http://numbersapi.com/"
 # app.route
 # def
 
@@ -9,10 +11,18 @@ token='Token'
 @app.route('/webhook', methods=["POST"])
 def api():
     req = request.get_json(silent=True, force=True)  # Standard
-    print(req)
-    print(request.form)
+  #  print(req)  # Prints full details
+    params = req["queryResult"]['parameters']
+    type_var = params.get('type')
+    num_var = params.get('number-integer')
+    print("Type: " + type_var)
+    print("Number: " + str(num_var))
+    target = url+str(int(num_var))+'/'+str(type_var)
+    print("Target URL: " + target)
+    res = requests.get(target)
+    ans = res.text
     response = {
-        "fulfillmentText": "WebHook Called"
+        "fulfillmentText": ans
     }
     return jsonify(response)
 
