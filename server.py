a8a6c7d3cee34fcad121d57e22eb0c990597bcc4
Susmit <susmit.py@gmail.com>
2019-03-23 12:02:58 +0530

    start

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask,request,jsonify
-from werkzeug.datastructures import ImmutableMultiDict
+# from werkzeug.datastructures import ImmutableMultiDict
 
 
 app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Good Susmit</h1>"
 
 
 
